f1097029cc2b93ead56be83a51c8d419f024d3e3
ShimmerPlume <94871035+ShimmerPlume@users.noreply.github.com>
2023-11-18 15:07:01 +0100

    Add: form for connexion

---
+++ app/front/CodeFront/src/app/connexion/connexion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-connexion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './connexion.component.html',
+  styleUrl: './connexion.component.css'
+})
+export class ConnexionComponent {
+  
+}

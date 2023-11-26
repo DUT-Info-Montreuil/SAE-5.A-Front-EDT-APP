3df665dea22ca92320073d6d0f6d835d3e78f6e5
Gilgamesh <Gilgamesh>
2023-11-26 16:55:58 +0100

    init utilisateur

---
+++ app/front/CodeFront/src/app/utilisateur/utilisateur.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-utilisateur',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './utilisateur.component.html',
+  styleUrl: './utilisateur.component.css'
+})
+export class UtilisateurComponent {
+
+}

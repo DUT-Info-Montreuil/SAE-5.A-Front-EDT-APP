a28d8d88236b244c823c0704290721db51d00920
Gilgamesh <Gilgamesh>
2023-12-01 00:05:11 +0100

    Ajout reponse formulaire addUtilisateur

--- app/front/CodeFront/src/app/form-creer-utilisateur/form-creer-utilisateur.component.ts
+++ app/front/CodeFront/src/app/form-creer-utilisateur/form-creer-utilisateur.component.ts
@@ -21,17 +21,42 @@ export class FormCreerUtilisateurComponent {
 		PassWord: ''
     };
 	
-	constructor(private router: Router, private axiosService: AxiosService) { }
+  successMessage: string = '';
+  errorMessage: string = '';
+  constructor(private router: Router, private axiosService: AxiosService) { }
 	
   redirectToPage() {
 	this.router.navigate(['/menu-utilisateur']);
   }
   
   onSubmit() {
-    this.axiosService.postData(this.formData).subscribe(response => {
-      console.log('Réponse de la requête POST :', response);
-      // Gérez la réponse comme nécessaire
-    });
+	  
+	const FirstName = this.formData.FirstName;
+    const LastName = this.formData.LastName;
+	const IdUtilisateur = this.formData.IdUtilisateur;
+    const Username = this.formData.Username;
+	const PassWord = this.formData.PassWord;
+
+    const postData = {
+      FirstName: FirstName,
+      LastName: LastName,
+	  IdUtilisateur: IdUtilisateur,
+      Username: Username,
+	  PassWord: PassWord,
+    };
+	
+    this.axiosService.postData(postData).subscribe(
+      response => {
+        console.log('Réponse de la requête POST :', response);
+        this.successMessage = 'Formulaire envoyé avec succès!';
+        this.errorMessage = ''; // Effacer le message d'erreur s'il y en avait un
+      },
+      error => {
+        console.error('Erreur lors de la requête POST :', error);
+        this.successMessage = ''; // Effacer le message de succès s'il y en avait un
+        this.errorMessage = 'Une erreur s\'est produite lors de l\'envoi du formulaire.';
+      }
+    );
   }
 }
 

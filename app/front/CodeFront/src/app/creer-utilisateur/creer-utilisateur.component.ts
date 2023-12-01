c6b548f6b3fc52491842b3fbec39d93ad0c64531
THEfireZz <95474704+THEfireZz@users.noreply.github.com>
2023-12-01 15:28:35 +0000

    Add creer-utilisateur component

---
+++ app/front/CodeFront/src/app/creer-utilisateur/creer-utilisateur.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-creer-utilisateur',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './creer-utilisateur.component.html',
+  styleUrl: './creer-utilisateur.component.css'
+})
+export class CreerUtilisateurComponent {
+
+}

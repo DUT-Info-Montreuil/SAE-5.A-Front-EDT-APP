d0a4bb4c47a402d0d3781e43d537aa36f3e02b4a
THEfireZz <maximedhaeyere@gmail.com>
2023-12-15 00:07:43 +0100

    Remove unused import and component declaration

--- app/front/CodeFront/src/app/app.module.ts
+++ app/front/CodeFront/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormCreerUtilisateurComponent } from './form-creer-utilisateur/form-creer-utilisateur.component'; // Importez votre composant
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormCreerUtilisateurComponent // Ajoutez votre composant ici
   ],
   imports: [
     BrowserModule,

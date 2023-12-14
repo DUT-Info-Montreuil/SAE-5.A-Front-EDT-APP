019020db2a32f1a7915ebc9132b70e26c65df962
THEfireZz <maximedhaeyere@gmail.com>
2023-12-14 14:36:05 +0100

    Remove unused files and update routes

--- app/front/CodeFront/src/app/app.routes.ts
+++ app/front/CodeFront/src/app/app.routes.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
-import { FormCreerUtilisateurComponent } from './form-creer-utilisateur/form-creer-utilisateur.component';
+
 import { FormTrouverUtilisateurComponent } from './form-trouver-utilisateur/form-trouver-utilisateur.component';
 import { MenuUtilisateurComponent } from './menu-utilisateur/menu-utilisateur.component';
-import { CreerUtilisateurComponent } from './creer-utilisateur/creer-utilisateur.component';
 import { CreateUserArrayComponent } from './create-user-array/create-user-array.component';
 
 
 
 export const routes: Routes = [
   {path: 'connexion', component: ConnexionComponent},
-	{ path: 'form-creer-utilisateur', component: FormCreerUtilisateurComponent },
 	{ path: 'form-trouver-utilisateur', component: FormTrouverUtilisateurComponent },
 	{ path: 'menu-utilisateur', component: MenuUtilisateurComponent },
-  { path: 'creer-utilisateur', component: CreerUtilisateurComponent },
   { path: 'create-user-array', component: CreateUserArrayComponent },
   { path: '', redirectTo: '/connexion', pathMatch: 'full' },
 

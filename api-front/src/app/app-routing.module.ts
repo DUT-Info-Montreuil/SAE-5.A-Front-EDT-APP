e4665df4280b401db6eb54388351f0249c117b5b
THEfireZz <maximedhaeyere@gmail.com>
2023-12-17 21:39:42 +0100

    Add CreateUsersComponent to app-routing.module.ts and app.module.ts

--- api-front/src/app/app-routing.module.ts
+++ api-front/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 /* import components */
 import { CalendarComponent } from './calendar/calendar.component';
+import { CreateUsersComponent } from './create-users/create-users.component';
 
 
 
 export const appRoutes: Routes = [
   { path: 'calendar', component: CalendarComponent },
+  { path: 'create-users', component: CreateUsersComponent }
 ];
 
 @NgModule({
--- api-front/src/app/app.module.ts
+++ api-front/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,12 +7,17 @@ import { AppComponent } from './app.component';
 import  { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarComponent } from './calendar/calendar.component';
+import { CreateUsersComponent } from './create-users/create-users.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    CreateUsersComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -20,7 +26,11 @@ import { CalendarComponent } from './calendar/calendar.component';
       useFactory: adapterFactory,
     }),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

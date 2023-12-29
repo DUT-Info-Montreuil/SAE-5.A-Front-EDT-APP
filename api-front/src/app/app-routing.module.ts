366dc89220477910f693f4b7630b9106a9a61474
Armand <95768839+Armandpro12@users.noreply.github.com>
2023-12-29 16:02:22 +0100

    add sticky navbar (buttons not working for now)

--- api-front/src/app/app-routing.module.ts
+++ api-front/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 /* import components */
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {NavbarComponent} from "./components/navbar/navbar.component";
 
 export const appRoutes: Routes = [
   { path: 'connexion', component: ConnexionComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'navbar', component: NavbarComponent }
 ];
 
 @NgModule({
--- api-front/src/app/components/navbar/navbar.component.ts
+++ api-front/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+/*  animations: [
+    trigger('openClose', [
+      state('true', style({ width: 'auto' })),
+      state('false', style({ width: '0px' })),
+      transition('false <=> true', animate(500))
+    ])
+  ]*/
 })
 export class NavbarComponent {
 
+  isOpen = true;
+  toggleNavBar(){
+    if(this.isOpen){
+      this.isOpen = false;
+    }
+    else{
+      this.isOpen = true;
+    }
+  }
 }
+
+

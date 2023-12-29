299f2c88dc92053f9986b87d64f515ed977db7a2
ShimmerPlume <94871035+ShimmerPlume@users.noreply.github.com>
2023-12-29 21:36:43 +0100

    Feat: change web site

--- api-front/src/main.ts
+++ api-front/src/main.ts
@@ -8,15 +8,19 @@ let subscription: Subscription;
 // Every time (in ms) check if token is here else redirect exept if already on connexion
 let time = 1000
 const source = interval(time);
-subscription = source.subscribe(val => returnToLogin());
+subscription = source.subscribe(val => redirectToLogin());
 
-function returnToLogin() {
+function redirectToLogin() {
   if (!window.localStorage.getItem("token") && document.location.pathname!="/connexion")
     document.location.pathname ="/connexion";
 }
-function changePage(pathname:string) {  
+function changePage(pathname:string, changeWebSite:boolean) {  
+  if (changeWebSite){
+    document.location.href = pathname
+    return;
+  }
   // if no token return to login page
-  returnToLogin()
+  redirectToLogin()
   document.location.pathname = pathname;
   setTimeout(() => console.log("test nav"), 1000)
 }

0828936c6aa9a07de468ac6f89eaa1766a59ad08
THEfireZz <maximedhaeyere@gmail.com>
2024-01-15 23:06:53 +0100

    Add FormsModule to ProfileModule and implement date selection functionality in ProfileComponent

--- api-front/src/app/components/profile/profile.component.ts
+++ api-front/src/app/components/profile/profile.component.ts
@@ -7,6 +7,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+    currentDate = new Date();
+    dateOptions: { label: string; value: string }[] = [];
+    selectedDate: string = '';
+
 
     user = {
       FirstName: '',
@@ -21,18 +25,20 @@ export class ProfileComponent {
         groupe: '',
       }
     };
-    //role :any = {};
     monthlyWorkedHours: any = {};
     
     constructor(private http: HttpClient) { 
-      this.getLoggedUser().then((data: any) => {
-        this.user = data;
-        console.log(this.user);
-        this.getMonthlyWorkedHours();
-      }
-      );
-        
-        //this.getMonthlyWorkedHours();
+
+    }
+
+    ngOnInit(): void {
+        this.generateYearList();
+        this.selectedDate = this.getDefaultDate();
+        console.log(this.dateOptions);
+        this.getLoggedUser().then((data: any) => {
+            this.user = data;
+            this.getMonthlyWorkedHours();
+        });
     }
 
 
@@ -52,21 +58,55 @@ export class ProfileComponent {
         
       }
 
-  getMonthlyWorkedHours(): any {
-    const token = localStorage.getItem('token');
-    const headers = { 'Authorization': `Bearer ${token}` };
-    const id = this.user.role.id;
-    const body = { 
-      mois: "2024-01"
-     };
-    this.http.post('http://localhost:5050/utilisateurs/getTeacherHoursInMonth/' + id, body, { headers }).subscribe({
-      next: (data: any) => {
-        this.monthlyWorkedHours = data;
-        console.log("data" + data);
-      },
-      error: (error: any) => {
-        console.log(error);
+    getMonthlyWorkedHours(): any {
+        const token = localStorage.getItem('token');
+        const headers = { 'Authorization': `Bearer ${token}` };
+        const id = this.user.role.id;
+        const body = { 
+        mois: this.selectedDate
+        };
+        this.http.post('http://localhost:5050/utilisateurs/getTeacherHoursInMonth/' + id, body, { headers }).subscribe({
+        next: (data: any) => {
+            this.monthlyWorkedHours = data;
+            console.log("data" + data);
+        },
+        error: (error: any) => {
+            console.log(error);
+        }
+        });
+    }
+
+  generateYearList(): void {
+    const currentYear = new Date().getFullYear();
+    const startYear = currentYear - 3;
+    const endYear = currentYear + 1;
+
+    for (let year = startYear; year <= endYear; year++) {
+        for (let month = 1; month <= 12; month++) {
+          const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+          const value = `${year}-${formattedMonth}`;
+          const label = `${this.monthName(month)} ${year}`;
+          this.dateOptions.push({ label, value });
+        }
       }
-    });
   }
+
+  monthName(month: number): string {
+    const options: Intl.DateTimeFormatOptions = { month: 'long' };
+    return new Intl.DateTimeFormat('fr-FR', options).format(new Date(2000, month - 1, 1));
+  }
+
+  getDefaultDate(): string {
+    const currentYear = this.currentDate.getFullYear();
+    const currentMonth = this.currentDate.getMonth() + 1; // Les mois vont de 0 à 11
+
+    return `${currentYear}-${currentMonth < 10 ? '0' : ''}${currentMonth}`;
+  }
+
+  changeDate(event:any): void {
+    this.selectedDate = event.target.value;
+    this.getMonthlyWorkedHours();
+  }
+
+
 }
--- api-front/src/app/components/profile/profile.module.ts
+++ api-front/src/app/components/profile/profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { ProfileComponent } from './profile.component';
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class ProfileModule {

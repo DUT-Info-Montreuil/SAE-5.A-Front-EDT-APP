11d4f0e09119f55051c51e6fc567bef1cb41acd4
THEfireZz <maximedhaeyere@gmail.com>
2024-01-16 12:46:24 +0100

    Add modal for creating a course

--- api-front/src/app/components/edt-calendar/edt-calendar.component.ts
+++ api-front/src/app/components/edt-calendar/edt-calendar.component.ts
@@ -1,9 +1,9 @@
 import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  Injectable,
-  ViewEncapsulation,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Injectable,
+    ViewEncapsulation,
 } from '@angular/core';
 
 import { CalendarEvent, CalendarEventTitleFormatter } from 'angular-calendar';
@@ -11,111 +11,185 @@ import { WeekViewHourSegment } from 'calendar-utils';
 import { fromEvent } from 'rxjs';
 import { finalize, takeUntil } from 'rxjs/operators';
 import { addDays, addMinutes, endOfWeek } from 'date-fns';
+import { HttpClient } from '@angular/common/http';
 
 function floorToNearest(amount: number, precision: number) {
-  return Math.floor(amount / precision) * precision;
+    return Math.floor(amount / precision) * precision;
 }
 
 function ceilToNearest(amount: number, precision: number) {
-  return Math.ceil(amount / precision) * precision;
+    return Math.ceil(amount / precision) * precision;
 }
 
 @Injectable()
 export class CustomEventTitleFormatter extends CalendarEventTitleFormatter {
-  override weekTooltip(event: CalendarEvent<any>, title: string): string {
-    if (!event.meta.tmpEvent) {
-      return super.weekTooltip(event, title) || '';
+    override weekTooltip(event: CalendarEvent<any>, title: string): string {
+        if (!event.meta.tmpEvent) {
+            return super.weekTooltip(event, title) || '';
+        }
+        return '';
     }
-    return '';
-  }
 
-  override dayTooltip(event: CalendarEvent<any>, title: string): string {
-    if (!event.meta.tmpEvent) {
-      return super.dayTooltip(event, title) || '';
+    override dayTooltip(event: CalendarEvent<any>, title: string): string {
+        if (!event.meta.tmpEvent) {
+            return super.dayTooltip(event, title) || '';
+        }
+        return '';
     }
-    return '';
-  }
 }
 
 @Component({
-  selector: 'app-edt-calendar',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: './edt-calendar.component.html',
-  styleUrls: ['./edt-calendar.component.css'],
-  providers:[
-    {
-      provide: CalendarEventTitleFormatter,
-      useClass: CustomEventTitleFormatter,
-    }
-  ],
-  styles: [
-    '.disable-hover { pointer-events: none; }',
-  ],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-edt-calendar',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './edt-calendar.component.html',
+    styleUrls: ['./edt-calendar.component.css'],
+    providers: [
+        {
+            provide: CalendarEventTitleFormatter,
+            useClass: CustomEventTitleFormatter,
+        }
+    ],
+    styles: [
+        '.disable-hover { pointer-events: none; }',
+    ],  
+    encapsulation: ViewEncapsulation.None,
 })
 export class EdtCalendarComponent {
-  viewDate = new Date();
-  events: CalendarEvent[] = [];
-  dragToCreateActive = false;
-  weekStartsOn: 0 = 0;
-  dayStartHour: number = 8;
-  dayEndHour: number = 19;
+    toggleSuprRessource() {
+        this.showSuprRessource = !this.showSuprRessource;
+    }
+    showSuprRessource = false;
+    viewDate = new Date();
+    events: CalendarEvent[] = [];
+    dragToCreateActive = false;
+    weekStartsOn: 0 = 0;
+    dayStartHour: number = 8;
+    dayEndHour: number = 19;
 
-  constructor(private cdr: ChangeDetectorRef) {}
+    ressouces: any[] = [];
+    groupes: any[] = [];
+    salles: any[] = [];
+    teachers: any[] = [];
 
-  startDragToCreate(
-    segment: WeekViewHourSegment,
-    mouseDownEvent: MouseEvent,
-    segmentElement: HTMLElement
-  ) {
-    const dragToSelectEvent: CalendarEvent = {
-      id: this.events.length,
-      title: 'New event',
-      start: segment.date,
-      meta: {
-        tmpEvent: true,
-      },
-    };
-    this.events = [...this.events, dragToSelectEvent];
-    const segmentPosition = segmentElement.getBoundingClientRect();
-    this.dragToCreateActive = true;
-    const endOfView = endOfWeek(this.viewDate, {
-      weekStartsOn: this.weekStartsOn,
-    });
+    constructor(private cdr: ChangeDetectorRef, private http: HttpClient) { }
 
-    fromEvent(document, 'mousemove')
-      .pipe(
-        finalize(() => {
-          delete dragToSelectEvent.meta.tmpEvent;
-          this.dragToCreateActive = false;
-          this.refresh();
-        }),
-        takeUntil(fromEvent(document, 'mouseup'))
-      )
-      .subscribe((mouseMoveEvent: Event) => {
-        const mouseEvent = mouseMoveEvent as MouseEvent;
+    startDragToCreate(
+        segment: WeekViewHourSegment,
+        mouseDownEvent: MouseEvent,
+        segmentElement: HTMLElement
+    ) {
 
-        const minutesDiff = ceilToNearest(
-          mouseEvent.clientY - segmentPosition.top,
-          30
-        );
+        const dragToSelectEvent: CalendarEvent = {
+            id: this.events.length,
+            title: '',
+            start: segment.date,
+            meta: {
+                tmpEvent: true,
+            },
+        };
 
-        const daysDiff =
-          floorToNearest(
-            mouseEvent.clientX - segmentPosition.left,
-            segmentPosition.width
-          ) / segmentPosition.width;
+        this.events = [...this.events, dragToSelectEvent];
+        const segmentPosition = segmentElement.getBoundingClientRect();
+        this.dragToCreateActive = true;
+        const endOfView = endOfWeek(this.viewDate, {
+            weekStartsOn: this.weekStartsOn,
+        });
 
-        const newEnd = addDays(addMinutes(segment.date, minutesDiff), daysDiff);
-        if (newEnd > segment.date && newEnd < endOfView) {
-          dragToSelectEvent.end = newEnd;
+        fromEvent(document, 'mousemove')
+            .pipe(
+                finalize(() => {
+                    console.log("event" );
+                    console.log(dragToSelectEvent.start.toLocaleDateString());
+                    delete dragToSelectEvent.meta.tmpEvent;
+                    this.dragToCreateActive = false;
+                    this.refresh();
+                    this.getAvailableRessources();
+
+                    console.log(this.ressouces);
+                    //this.showSuprRessource = true;
+
+                }),
+                takeUntil(fromEvent(document, 'mouseup'))
+            )
+            .subscribe((mouseMoveEvent: Event) => {
+                const mouseEvent = mouseMoveEvent as MouseEvent;
+
+                const minutesDiff = ceilToNearest(
+                    mouseEvent.clientY - segmentPosition.top,
+                    30
+                );
+
+                const daysDiff =
+                    floorToNearest(
+                        mouseEvent.clientX - segmentPosition.left,
+                        segmentPosition.width
+                    ) / segmentPosition.width;
+
+                const newEnd = addDays(addMinutes(segment.date, minutesDiff), daysDiff);
+                if (newEnd > segment.date && newEnd < endOfView) {
+                    dragToSelectEvent.end = newEnd;
+                }
+                this.refresh();
+            });
+
+    }
+
+
+    private refresh() {
+        this.events = [...this.events];
+        this.cdr.detectChanges();
+    }
+
+
+    getAvailableRessources() {
+        const token = localStorage.getItem('token');
+        const headers = { 'Authorization': `Bearer ${token}` };
+
+        this.http.get('http://localhost:5050/ressource/getDispo', { headers }).subscribe({
+        next: (data: any) => {
+            console.log(data);
+            this.ressouces = data;
+        },
+        error: (error: any) => {
+            console.log(error);
         }
-        this.refresh();
-      });
-  }
+        });
+    }
 
-  private refresh() {
-    this.events = [...this.events];
-    this.cdr.detectChanges();
-  }
+    getAvailableTeacher() {
+        const token = localStorage.getItem('token');
+        const headers = { 'Authorization': `Bearer ${token}` };
+        const body = { 
+            //NombreHeure: type Time (HH:MM:SS)
+            //Jour: type string (YYYY-MM-DD)
+            //HeureDebut: type Time (HH:MM:SS)
+            'HeureDebut': this.events[0].start,
+            'Jour': this.events[0].start.getDay(),
+            'NombreHeure': '',
+         };
+        this.http.post('http://localhost:5050/teacher/getDispo', body, { headers }).subscribe({
+            next: (data: any) => {
+                console.log(data);
+                this.teachers = data;
+            },
+            error: (error: any) => {
+                console.log(error);
+            }
+        });
+        
+    }
+    getAvailableRoom() {
+        return this.events.filter(event => event.meta.tmpEvent);
+    }
+    getAvailableGroupe() {
+        return this.events.filter(event => event.meta.tmpEvent);
+    }
+
+
+
+}
+
+interface customeCoursEvent extends CalendarEvent {
+
+
 }
